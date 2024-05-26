48f18a5ab76cf2913d77ad94c86138d1e14a367e
febits <febits@proton.me>
2024-05-26 18:16:57 -0300

    avoiding arrows keys from escape the current line

--- src/client/input.c
+++ src/client/input.c
@@ -1,13 +1,19 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <termios.h>
 #include <stdbool.h>
+#include <stdio.h>
+#include <termios.h>
+#include <unistd.h>
 
 #include "types.h"
 
 #define DEL 0x7f
 #define BS 0x08
 #define NAK 0x15
+#define ESC 0x1b
+
+#define ARROW_UP 0x41
+#define ARROW_DOWN 0x42
+#define ARROW_RIGHT 0x43
+#define ARROW_LEFT 0x44
 
 bool noncanon_input(char *dst, u64 max) {
   struct termios old = {0};
@@ -30,10 +36,26 @@ bool noncanon_input(char *dst, u64 max) {
       return false;
     }
 
+    // break and continue points
     if (c == '\n') {
       break;
+    } else if (c == ESC) {
+      char ansi_codes[2] = {0};
+      if (read(STDIN_FILENO, ansi_codes, 2) == 2 && ansi_codes[0] == '[') {
+        switch (ansi_codes[1]) {
+          case ARROW_UP:
+          case ARROW_DOWN:
+          case ARROW_LEFT:
+          case ARROW_RIGHT:
+            continue;
+        }
+      } else {
+        tcsetattr(STDIN_FILENO, TCSANOW, &old);
+        return false;
+      }
     }
 
+    // safe to interpret points
     if ((c == DEL || c == '\b' || c == BS) && i > 0) {
       dst[--i] = '\0';
       printf("\b \b");
